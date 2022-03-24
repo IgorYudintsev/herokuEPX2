fc3356fe83278ee683452b845cdfcdf0994da09e
IgorYudintsev <kakvospitat@gmail.com>
2022-03-24 12:09:05 +0300

    relokete to src3

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import './App.css';
 import Header from "./components/Header";
 import {Footer} from "./components/Footer";
 import Content from "./components/Content";
+import {Navigation} from "./components/Navigation";
 
 function App() {
-    let [cargoOfskills, setCargoOfskills] = useState ([
+    let [cargoOfskills, setCargoOfskills] = useState([
         {id: 1, skill: "HTML&CSS", isDone: true},
         {id: 2, skill: "Flex", isDone: true},
         {id: 3, skill: "GRID", isDone: false},
@@ -13,17 +14,14 @@ function App() {
         {id: 5, skill: "JS", isDone: true},
     ])
 
-    // const removeSkill = (id:number) =>{
-    //    setCargoOfskills(cargoOfskills.filter(f=>f.id !==id))
-    // }
-
     const arrForFooterHeder = ['HEADER', 'FOOTER']
-
     return (
         <div>
             <Header name={'header'}/>
-            <Content skills1={cargoOfskills} cargoOfskills={cargoOfskills} setCargoOfskills={setCargoOfskills} />
-
+            <div className={'Body'}>
+                <Navigation/>
+                <Content skills1={cargoOfskills} cargoOfskills={cargoOfskills} setCargoOfskills={setCargoOfskills}/>
+            </div>
             <Footer name={arrForFooterHeder}/>
         </div>
 
